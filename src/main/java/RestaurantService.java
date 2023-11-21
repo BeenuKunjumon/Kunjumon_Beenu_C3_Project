33b4d7fd997ed2b61724b19e14a09b03909b4f79
Beenu Kunjumon <beenuk126@gmail.com>
2023-11-21 20:39:20 +0530

    Part 3: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -1,9 +1,18 @@
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
+    private static final Map<String, Double> menuItemsAndPrice = new HashMap<>();
+
+    public RestaurantService(){
+        menuItemsAndPrice.put("Sweet Corn Soup", 5.0);
+        menuItemsAndPrice.put("Vegetable Lasagne", 15.0);
+        menuItemsAndPrice.put("Chicken Biriyani", 50.0);
+    }
 
     public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException{
 
@@ -31,5 +40,17 @@ public class RestaurantService {
 
     public List<Restaurant> getRestaurants() {
         return restaurants;
+    }
+
+    public Double calculateOrderValue(String... menuItems){
+
+        Double totalCost = 0.0;
+
+        for(String item : menuItems){
+            if(menuItemsAndPrice.containsKey(item)){
+                totalCost += menuItemsAndPrice.get(item);
+            }
+        }
+        return totalCost;
     }
 }
